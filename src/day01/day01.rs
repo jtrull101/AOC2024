40dc8ea2ae3b42c41659fa598d4cb5b48f3867bd
jtrull101 <33765387+jtrull101@users.noreply.github.com>
2024-12-01 16:23:19 -0500

    push part 2 of Day 01

--- src/day01/day01.rs
+++ src/day01/day01.rs
@@ -1,7 +1,43 @@
 use std::collections::BinaryHeap;
+use std::collections::HashMap;
 use std::cmp::Reverse;
 
-pub fn solve() {
+
+pub fn solve_part1() {
+    let (mut heap1, mut heap2) = setup_heaps();
+
+    let mut sum = 0;
+    while let (Some(Reverse(val1)), Some(Reverse(val2))) = (heap1.pop(), heap2.pop()) {
+        sum += (val2 - val1).abs();
+    }
+
+    println!("Day 01 part 1: {}", sum);
+}
+
+
+pub fn solve_part2() {
+    let (mut heap1, mut heap2) = setup_heaps();
+    let mut found_values: HashMap<i32, i32> = HashMap::new();
+
+    while let Some(Reverse(val1)) = heap1.pop() {
+        let mut similarity_score = 0;
+        for &Reverse(val2) in heap2.iter() {
+            if val1 == val2 {
+                similarity_score += val1;
+            }
+        }
+        found_values.insert(val1, similarity_score);
+    }
+
+    let sum: i32 = found_values.values().sum();
+    println!("Day 01 part 2: {}", sum);
+}
+
+/**
+ * Read the input file and create two heaps.
+ * The heaps are sorted in ascending order.
+ */
+fn setup_heaps() -> (BinaryHeap<Reverse<i32>>, BinaryHeap<Reverse<i32>>) {
     let input = include_str!("../../input/day01.txt");
     let mut heap1: BinaryHeap<Reverse<i32>> = BinaryHeap::new();
     let mut heap2: BinaryHeap<Reverse<i32>> = BinaryHeap::new();
@@ -14,10 +50,5 @@ pub fn solve() {
         heap2.push(Reverse(num2));
     }
 
-    let mut sum = 0;
-    while let (Some(Reverse(val1)), Some(Reverse(val2))) = (heap1.pop(), heap2.pop()) {
-        sum += (val2 - val1).abs();
-    }
-
-    println!("Day 01: {}", sum);
+    (heap1, heap2)
 }
--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -1,5 +1,6 @@
 pub mod day01;
 
 pub fn run() {
-    day01::solve();
+    day01::solve_part1();
+    day01::solve_part2();
 }
